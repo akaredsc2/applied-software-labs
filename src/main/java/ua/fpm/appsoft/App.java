b72536247eebab2d525f3290f70c202e4f1c436e
Vitaly <akaredsc2@gmail.com>
2016-09-19 18:47:32 +0300

    initial commit

---
+++ src/main/java/ua/fpm/appsoft/App.java
@@ -0,0 +1,13 @@
+package ua.fpm.appsoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
